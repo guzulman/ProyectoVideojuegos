ba02d07ec6891eee601e688a958cbb9b65a82618
sebasrodrip <106126186+sebasrodrip@users.noreply.github.com>
2023-07-31 18:25:35 -0600

    Adicion del enemigos
    Y animaciones

---
+++ Assets/Scripts/PatrolController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolController : MonoBehaviour
+{
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    bool isFacingRight;
+
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D raycastHit2D =
+            Physics2D.Raycast(groundCheck.position, Vector2.down, 0.15F);
+
+        if (!raycastHit2D)
+        {
+            FlipX();
+        }
+
+        _rb.velocity = new Vector2(speed * Time.fixedDeltaTime, _rb.velocity.y);
+
+    }
+
+    private void FlipX()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0F, 180.0F, 0.0F);
+        speed *= -1;
+    }
+}
---
+++ Assets/Scripts/PatrolStateController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml.Serialization;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class PatrolStateController : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] points;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    bool isFacingRight;
+
+    Rigidbody2D _rb;
+
+    int _index;
+
+    Transform _target;
+
+
+
+    private void Start()
+    {
+        SetNextTarget(0);
+    }
+
+    private void Update()
+    {
+        if (transform.position == _target.position)
+        {
+            _index++;
+
+            if (_index >= points.Length)
+            {
+                _index = 0;
+            }
+            SetNextTarget(_index);
+        }
+
+        transform.position =
+            Vector2.MoveTowards(transform.position, _target.position, speed * Time.deltaTime);  
+    }
+    private void SetNextTarget(int index)
+    {
+        _index = index;
+        _target = points[_index];
+
+        if (_target.position.x > transform.position.x && !isFacingRight)
+        {
+            if (!isFacingRight)
+            {
+                isFacingRight = !isFacingRight;
+                transform.Rotate(0.0F, 180.0F, 0.0F);
+            }
+        }
+        else if(_target.position.x < transform.position.x && isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0.0F, 180.0F, 0.0F);
+        }
+    }
+}
+
